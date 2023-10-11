64f586b60691c0f61d8eab606a155cf7fb121336
Eliana Puerta <80785262+Eliana-Janneth@users.noreply.github.com>
2023-10-10 20:27:04 -0500

    feat: consult animal medical history

--- src/components/elements/VDetails.vue
+++ src/components/elements/VDetails.vue
@@ -1,18 +1,24 @@
 <script setup lang="ts">
+
+import { useUserStore } from '@/stores';
+
 defineProps<{
     customClass?: string;
     label: string;
     description: string;
 }>();
+
+const userStore = useUserStore();
+
 </script>
 
 <template>
-    <div class="w-full border border-x-0 border-t-0 border-sky-200/50">
+    <div :class="['w-full border border-x-0 border-t-0', userStore.isVet ? 'border-sky-200/50' : 'border-emerald-200/50']">
         <dl class="p-2">
             <div class="flex flex-col">
-                <dt class="text-lg font-medium text-sky-600">{{ label }}</dt>
+                <dt :class="['text-lg font-medium',userStore.isFarmer ? 'text-emerald-600': 'text-sky-600']">{{ label }}</dt>
                 <dd class="ml-2 text-justify">
-                    <span :class="['text-base font-normal text-sky-800', customClass]">{{ description }}</span>
+                    <span :class="['text-base font-normal',userStore.isFarmer ? 'text-emerald-800': 'text-sky-800', customClass]">{{ description }}</span>
                 </dd>
             </div>
         </dl>
