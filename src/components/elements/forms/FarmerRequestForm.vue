1916056544702d0a9a00797a7b007e80516b8943
Mateo Velásquez Rodríguez <mateo.velasquezr@udea.edu.co>
2023-11-13 21:28:34 -0500

    feat: language farmer request

--- src/components/elements/forms/FarmerRequestForm.vue
+++ src/components/elements/forms/FarmerRequestForm.vue
@@ -5,14 +5,17 @@ import { TRegisterFarmerRequestPayload, vetappApi } from '@/services';
 import type { TRegisterFarmerRequest } from '@/types';
 import * as yup from 'yup';
 import { useForm } from 'vee-validate';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
 
 const props = defineProps<{
     idVet: string;
 }>();
 
 const validationSchema = yup.object({
-    animal: yup.string().required('Por favor seleccione un animal'),
-    message: yup.string().required('Por favor ingrese un mensaje'),
+    animal: yup.string().required(t('FarmPage.valconsulanimal')),
+    message: yup.string().required(t('FarmPage.valwritesol2')),
 });
 
 const { handleSubmit, isSubmitting } = useForm<TRegisterFarmerRequest>({
@@ -47,7 +50,7 @@ const onSubmit = handleSubmit(async (values: TRegisterFarmerRequest) => {
     <form class="grid grid-cols-1 gap-4 sm:grid-cols-8" @submit="onSubmit">
         <VTextArea
             containerClass="sm:col-span-8"
-            label="Escribir mensaje"
+            :label="t('FarmPage.writesol')"
             name="message"
             placeholder="Escribe el motivo por el cual quieres realizar una consulta con el veterinario"
         />
@@ -55,16 +58,16 @@ const onSubmit = handleSubmit(async (values: TRegisterFarmerRequest) => {
             containerClass="sm:col-span-8"
             placeholder="Seleccione el animal"
             name="animal"
-            label="Animal a consultar"
+            :label="t('FarmPage.consulanimal')"
             :options="animals"
         />
-        <VButton class="w-full sm:col-span-4" @click="$emit('end')" label="Cancelar" variant="danger" />
+        <VButton class="w-full sm:col-span-4" @click="$emit('end')" :label="t('VetPage.cancel')" variant="danger" />
         <VButton class="w-full sm:col-span-4" type="submit" variant="success">
             <div v-if="isSubmitting" class="flex items-center gap-2">
                 <VLoader class="h-6" />
-                <span>Enviando</span>
+                <span>{{ $t("VetPage.send") }}</span>
             </div>
-            <template v-else> Enviar Solicitud </template>
+            <template v-else>{{ $t("FarmPage.sendsol") }}</template>
         </VButton>
     </form>
 </template>
